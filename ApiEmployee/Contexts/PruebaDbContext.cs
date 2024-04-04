a67c6384cd2f6a48dee1c5b0c818f29b6fb5c2b9
Jesus David Muñoz Farfan <jdmunozf97@gmail.com>
2024-04-04 13:34:59 -0500

    feat[app]: add dbcontext

---
+++ ApiEmployee/Contexts/PruebaDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEmployee.Contexts
+{
+    public class PruebaDbContext(DbContextOptions<PruebaDbContext> options) : DbContext(options), IEmployeeContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
---
+++ Infrastructure/Entities/UserEntity.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Entities
+{
+    [Table("Users")]
+    public class UserEntity : BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required, Key]
+        public int Id { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public required String Email { get; set; }
+
+        public required String Name { get; set; }
+
+        public required String LastName { get; set; }
+
+        public required String Password { get; set; }
+    }
+}
